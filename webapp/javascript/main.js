7c0740f551628dce7b5484d487cc53ca4fcb5a92
Deltachi <dominik.scheffler@me.com>
2015-05-18 01:46:22 +0200

    Made some Experiments with embedded videos and styles/frames/html5 vid objects. 90% failure. Video is eighter not behaving, or messes up the rest of the stylesheet.

--- webapp/javascript/main.js
+++ webapp/javascript/main.js
@@ -1,4 +1,8 @@
+var not_available_html = '<div id="alert-not-available"class="alert alert-danger important-alert" role="alert">Diese Funktion ist leider noch <strong>nicht verf&uuml;gbar! :(</strong></div>';
+
 $(function(){	
+	$("body").prepend(not_available_html);
+	$('#alert-not-available').hide()
 	$('.not-available').on('click',showAlert);
 
 	function showAlert(){
