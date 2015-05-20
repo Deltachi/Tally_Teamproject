a6ea1b55a0b5795bfe7ebcc671a2d687247327f3
Deltachi <dominik.scheffler@me.com>
2015-05-20 02:03:44 +0200

    new appeareance, lots of thinking to get the indexsss sorted, now functional. just the backend-communication left

--- webapp/javascript/favorites.js
+++ webapp/javascript/favorites.js
@@ -1,9 +1,40 @@
 $(function(){
-	
-	$('.deleteBtn').on('click', function(){
-		$button = $(this);
+	$favos = $('#favorites');
+	$addBtn = $('#addBtn');
+	$itemList = $('#itemList');
+	var list_index = 1;
+
+	$addBtn.on('click',function(){
+		var value = $itemList.val();
+		addItem(value);
+	});
+	function addItem(name){
+		var count =  $( "tr" ).length;
+		var htmlString = '<tr><th class="index" scope="row">'+count+'</th><td>'+name+'</td><td><button class="btn btn-sm btn-danger deleteBtn"><span class="glyphicon glyphicon-trash"></span></button></td></tr>';
+		$('#favorites > tbody:last').append(htmlString);
+		console.log('Added '+name+' at number '+count);
 		
-		$button.parent().parent().fadeOut(300);
+	}
+	$favos.delegate('.deleteBtn','click', function(){
+		console.log('Delete-button clicked');
+		$button = $(this);
+		$element = $button.closest('tr');
+		$list = $element.parent();
+		
+		list_index = 1;
+		$element.fadeOut(500,function(){
+			$(this).remove();
+			$('.index').each(function() {
+				var before = $(this).html();
+				var now = list_index;
+				$(this).html(now);
+				var item = $(this).next().html();
+				console.log(item+" -> before:"+before+" now:"+now);
+				list_index++;
+			});
+		});
+
+
 	});	
 	
 });
