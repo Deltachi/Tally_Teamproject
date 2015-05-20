0467680d50d99b2943238c794efebcb73688b8d4
Deltachi <dominik.scheffler@me.com>
2015-05-20 02:05:58 +0200

    integrated new tamplate and minor bug-fixes. changed classes, bc some links are available now

--- webapp/javascript/index.js
+++ webapp/javascript/index.js
@@ -1,18 +1,3 @@
 $(function(){
-	$('#alert-not-available').hide()
-	
-	$('#nav').affix({
-	      offset: {
-	        top: $('header').height()
-	      }
-	});
-	$('#btn_testLogin').on('click', function(){
-		$('#navLogin').html($('#username').val());
-		$('#navLogin').attr("href", "users.html");
-		$('#navLogin').attr("class", " ");
-		$('#menuLogin').attr("class", "dropdown");
-		$('#navLogin').attr("data-toggle", " ");
-		$('#panel_login').hide();
-	});	
 	
 });
