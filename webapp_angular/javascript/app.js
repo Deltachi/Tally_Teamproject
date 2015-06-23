e36192bc13e4deb3e46023727c3bd3649e0ed72d
Deltachi <dominik.scheffler@me.com>
2015-06-23 17:55:43 +0200

    all new system of handling things! set up the workspace, added files, migrated parts of old webapp to angular application

--- webapp_angular/javascript/app.js
+++ webapp_angular/javascript/app.js
@@ -1,20 +1,37 @@
-// $(function(){
-	var app = angular.module('app',[]);
-	app.directive('myNavbar', function(){
-		return {
-			restrict: 'E',
-			templateUrl: '/ng-templates/navbar.html'
-		};
-	});	
-	app.directive('welcome', function(){
-		return {
-			restrict: 'E',
-			templateUrl: '/ng-templates/title.html'
-		};
-	});
-	
-	app.controller('gemStore', ['', function(){
-		
-	}])
-	
-// });
+var app = angular.module('app',['ui.router']);
+
+app.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider, $stateProvider) {
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+		.state('/', {
+			url: '/',
+			templateUrl: 'ng-templates/home.html'
+		})
+		.state('home', {
+			url: '/home',
+			templateUrl: 'ng-templates/home.html'
+		})
+		.state('menu', {
+			url: '/menu',
+			templateUrl: 'ng-templates/menu.html'
+		})
+}]);
+
+app.directive('myNavbar', function(){
+	return {
+		restrict: 'E',
+		templateUrl: '/ng-templates/navbar.html'
+	};
+});	
+app.directive('login', function(){
+	return {
+		restrict: 'E',
+		templateUrl: '/ng-templates/login.html',
+	};
+});	
+app.directive('myFooter', function(){
+	return {
+		restrict: 'E',
+		templateUrl: '/ng-templates/footer.html'
+	};
+});	
