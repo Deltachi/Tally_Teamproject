689a48c427b6dee7982a0d477c2b8760f5fb63fa
Deltachi <dominik.scheffler@me.com>
2015-06-30 09:26:57 +0200

    This is... the back-end! added self testing php file

---
+++ webapp_angular/apis/users/dbtest.php
@@ -0,0 +1,29 @@
+<?php
+	try {
+	    // Use database file "database.sqlite" or create a new one if not exists:
+	    $db = new PDO('sqlite:../../sqlite/database.sqlite');
+	    // Throw exceptions on error
+	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	    // Create tables, if not exists:
+	    //$sql = "CREATE TABLE IF NOT EXISTS SensorValues (VID INTEGER PRIMARY KEY, TStamp TEXT, SName TEXT,	Value INTEGER, Sync INTEGER)";
+	    //$db->exec($sql);
+	    // Place INSERT, UPDATE or SELECT statements here!
+
+	    $query = "SELECT * FROM Users WHERE Firstname = 'Dominik'";
+		$results = $db->query($query);
+		$rows = $results->fetchAll(PDO::FETCH_ASSOC);
+		$data = array();
+		foreach ($rows as $rowKey => $row) {
+			$data[] = $row;
+			break;
+		}
+		echo json_encode($data);
+	    $db = NULL;		// Close database
+
+	} catch(PDOException $e) 	{
+	    echo "ERROR you noob :P -> ";
+	    echo $e->getMessage();
+	    echo $e->getTraceAsString();
+	}
+?>		
